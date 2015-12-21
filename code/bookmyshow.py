baf98a3ec1f16575fe2edc822e9e42c4a67fa735
phoenix911 <sangeet.verma91@gmail.com>
2015-12-21 09:47:16 +0530

    scrapping bookmyshow v0.2

--- code/bookmyshow.py
+++ code/bookmyshow.py
@@ -3,14 +3,26 @@ from bs4 import BeautifulSoup
 import os.path
 import pprint
 import re
-#os.chdir('bookmyshow')
+# os.chdir('bookmyshow')
 # url = 'http://in.bookmyshow.com/hyderabad/movies'
 r = requests.get('http://in.bookmyshow.com/hyderabad/movies').text
 d = BeautifulSoup(r,"html.parser")
 extract = d.findAll("a", "__movie-name").__str__()
 sim = re.split(r'</a>', extract)
-# s = str(extract)
-# file = open("buff/ap.txt","w")
-# file.write(s)
 matching = [s for s in sim if "data-id" in s]
-pprint.pprint(matching)
+# ### getting URL
+paturl = re.compile(r'href="+.*\"\s')
+# url = re.split(r'"', (re.findall(paturl, matching[1]))[0])[1]
+# ### getting title
+pattit = re.compile(r'title="+.*"')
+# title = re.split(r'"',(re.findall(pattit,matching[1]))[0])[1]
+
+tidict = {}
+for i in xrange(len(matching)):
+    title = re.split(r'"', (re.findall(pattit, matching[i]))[0])[1]
+    url = re.split(r'"', (re.findall(paturl, matching[i]))[0])[1]
+    tidict.update({title: url})
+print type(tidict)
+# finding your film name
+a = [value for key, value in tidict.items() if 'spec' in key.lower()]
+print a

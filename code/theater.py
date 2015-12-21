3cbb7cc7a6171e76583a28215f7f12db0138df3e
phoenix911 <sangeet.verma91@gmail.com>
2015-12-22 00:44:44 +0530

    date reminder

---
+++ code/theater.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from pydub import AudioSegment
+from pydub.playback import play
+import time
+#import os.path
+#print os.getcwd()
+
+#r = requests.get(').text
+
+song = AudioSegment.from_mp3("star-wars-theme-song.mp3")
+
+
+def notification(url):
+    r = requests.get(url).text
+    d = BeautifulSoup(r,"html.parser")
+    extract = d.findAll("div", "date-container").__str__()
+    da = re.findall(r'\D(\d{2})\D',extract)
+    print da
+    if '25' in da:
+        play(song)
+    else:
+        print 'hello'
+    time.sleep(15)
+
+# url = 'http://in.bookmyshow.com/buytickets/in-the-heart-of-the-sea-3d-hyderabad/movie-hyd-ET00036260-MT/20151222'
+url = 'http://in.bookmyshow.com/buytickets/star-wars-the-force-awakens-3d-hyderabad/movie-hyd-ET00025699-MT/20151224'
+
+while True:
+    notification(url)
+
